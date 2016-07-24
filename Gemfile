dd40059ce11cdfbeaf6f41fc60557dbc415e4a40
Matteo Scurati <matteo.scurati@gmail.com>
2016-07-24 16:35:04 +0200

    Start to style articles

--- Gemfile
+++ Gemfile
@@ -3,4 +3,5 @@ source 'https://rubygems.org'
 gem 'middleman', '>= 4.0.0'
 gem 'middleman-dato'
 gem "middleman-minify-html"
+gem "sanitize"
 gem "slim"
--- config.rb
+++ config.rb
@@ -11,12 +11,22 @@ page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
-# With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
+###
+# Dato configuration
+###
 
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
+activate :dato,
+  domain: 'admin-docs.lospaziodellapolitica.com',
+  token: '518d104e9d46d8536d8a124898efa5a38115baba501b4ba058',
+  base_url: 'https://docs.lospaziodellapolitica.com'
+
+all_articles = dato.articles.sort_by(&:position)
+
+proxy "/index.html", "/templates/index.html", locals: { articles: all_articles }
+
+dato.articles.each do |article|
+  proxy "/articles/#{article.slug}.html", "/templates/detail.html", locals: { article: article }
+end
 
 # Localization
 # activate :i18n
@@ -36,15 +46,6 @@ activate :external_pipeline,
   command: build? ? 'gulp build --production' : './node_modules/gulp/bin/gulp.js',
   source: "dist",
   latency: 1
-
-###
-# Dato configuration
-###
-
-activate :dato,
-  domain: 'admin-docs.lospaziodellapolitica.com',
-  token: '518d104e9d46d8536d8a124898efa5a38115baba501b4ba058',
-  base_url: 'https://docs.lospaziodellapolitica.com'
 
 set :url_root, 'https://docs.lospaziodellapolitica.com'
 
